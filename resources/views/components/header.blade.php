e4c0dde70c6c70053a7dd089b4e385137437363b
danes <dadadanesss@gmail.com>
2024-11-29 17:08:05 +0800

    initial commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,9 @@
+<div class="m-0 p-0">
+   <nav class="navbar navbar-light bg-white px-3 align-items-center shadow">
+        <div class="container-fluid">
+            <a class="navbar-brand text-primary d-flex align-items-center navbar-brand-responsive" href="/">
+                <span>DRRS <span class="d-none d-sm-inline">: Disaster Response & Recovery System</span></span>
+            </a>
+        </div>
+   </nav>
+</div>  
